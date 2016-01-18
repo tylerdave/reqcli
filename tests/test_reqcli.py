3a33912d8a1aeda3e5942c5a4d10969b1b599fa9
Dave Forgac <tylerdave@tylerdave.com>
2016-01-18 01:26:20 -0500

    testing command invocation

--- tests/test_reqcli.py
+++ tests/test_reqcli.py
@@ -9,17 +9,22 @@ Tests for `reqcli` module.
 """
 
 import unittest
+from click.testing import CliRunner
 
-import reqcli
+import reqcli.cli
 
 
 class TestReqcli(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.url = 'http://www.example.com/'
+        self.runner = CliRunner()
 
-    def test_hello_world_runs(self):
-        assert(reqcli.hello_world())
+    def test_basic_invocation(self):
+        result = self.runner.invoke(reqcli.cli.cli, [self.url])
+        assert result.exit_code == 0
+        # FIXME: mock out requests and assert based on mock response
+        # assert result.output == "Hello, world!\n"
 
     def tearDown(self):
         pass
